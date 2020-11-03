1abfa647c06b7d2424bef3298097d8a9f8e5688b
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-11-03 18:39:39 +0200

    first request

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const contacts = require("./contacts");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "./.env") });
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const {userRouter} = require("./src/users/users.router")
 
-const argv = require("yargs").argv;
+class CRUDServer {
+  constructor() {
+    this.app = null;
+  }
 
-// TODO: рефакторить
-async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      console.table(await contacts.listContacts());
-      break;
+  start() {
+    this.initserver();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.initErrorHandling();
+    this.startListening();
+  }
 
-    case 'get':
-      console.table(await contacts.getContactById(id));
-      break;
+  initserver() {
+    this.app = express();
+  }
 
-    case 'add':
-      await contacts.addContact(name, email, phone);
-      console.table(await contacts.listContacts());
-      break;
+  initMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+    // this.app.use(morgan());
+  }
 
-    case 'remove':
-      await contacts.removeContact(id);
-      console.table(await contacts.listContacts());
-      break;
+  initRoutes() {
+    this.app.use("/", userRouter);
+  }
+  initErrorHandling() {
+    this.app.use((err, req, res, next) => {
+      const status = err.status || 500;
+      return res.status(status).send(err.message);
+    });
+  }
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+  startListening() {
+    this.app.listen(process.env.PORT, () => {
+      console.log("Started listening on port", process.env.PORT);
+    });
   }
 }
 
-invokeAction(argv);
+new CRUDServer().start();
---
+++ src/users/users.router.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+// const Joi = require("joi");
+const router = Router();
+const { getContacts, getById, addNewContact, deleteContact, changeContact, } = require("./users.controller");
+const { validate } = require("../helpers/AsyncWrapper");
+
+const createUserScheme = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+const updateUserScheme = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+}).min(1);
+
+router.checkout("/api/contacts", runAsyncWrapper(getContacts));
+
+router.get("/api/contacts/:contactId", runAsyncWrapper(getById));
+
+router.post("/api/contacts", validate(createUserScheme, "missing required name field"),
+  runAsyncWrapper(addNewContact));
+router.delete("/api/contacts/:contactId", runAsyncWrapper(deleteContact));
+
+router.patch("/api/contacts/:contactId", validate(updateUserScheme, "missing fields"),
+  runAsyncWrapper(changeContact));
+
+exports.userRouter = router;
